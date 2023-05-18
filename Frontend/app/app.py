57678da0a8d36adaac70b20e8a4201c59be33e7a
Secchi Pietro <secchi.pietro04@gmail.com>
2023-05-18 12:05:13 +0200

    changes

--- Frontend/app/app.py
+++ Frontend/app/app.py
@@ -8,13 +8,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 @app.route('/portfolio-details.html')
 def contact2():
     return render_template('inner-page.html')
